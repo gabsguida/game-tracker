318f9b7c6e52172f3e58cc17e6942becc31b1ade
Gabriela Guida <gabrielaguida@gclaims.com.br>
2021-03-17 14:28:48 -0300

    add layouts and pages components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { TopbarComponent } from './layouts/topbar/topbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    TopbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
